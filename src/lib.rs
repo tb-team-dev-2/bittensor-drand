c38c2600f44c4c5866b99d98c3eec0b4624a278a
Roman <roman@opentensor.dev>
2024-12-02 09:57:30 -0800

    update `genesis_time`

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,7 @@ async fn generate_commit(
     // Calculate reveal_round
     // all of 3 variables are constants for drand quicknet
     let period = 3;
-    let genesis_time = 1689232296;
+    let genesis_time = 1692803367;
     let public_key = "83cf0f2896adee7eb8b5f01fcad3912212c437e0073e911fb90022d3e760183c8c4b450b6a0a6c3ac6a5776a2d1064510d1fec758c921cc22b0e17e63aaf4bcb5ed66304de9cf809bd274ca73bab4af5a6e9c76a4bc09e76eae8991ef5ece45a";
 
     let now = SystemTime::now()
